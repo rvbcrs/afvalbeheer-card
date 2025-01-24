8fb92848531908eb791e3839233a23ee8e69f9cf
Ramon van Bruggen <ramonvanbruggen@gmail.com>
2025-01-24 21:39:20 +0100

    Small build changes

--- webpack.config.js
+++ webpack.config.js
@@ -7,17 +7,12 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   mode: "production",
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
       },
     ],
   },
